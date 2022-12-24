ee8218b85051eaa2d640df519a16a30dc8fb6e59
ryanmcdonough <me@ryanmcdonough.co.uk>
2022-12-24 10:43:32 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ except Exception as e:
     long_description = "LexPredict LexNLP: A swiss-army knife library built for working with real, unstructured legal text."
 
 setup(
-    name='lexnlp',
+    name='openlegal-lexnlp',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
@@ -64,7 +64,7 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8',
 
         # Topics
         'Natural Language :: English',
